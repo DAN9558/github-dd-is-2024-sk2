932e770ba0768fc44a896bd999bdd6e9f316c2ab
Daniel Dubský <daniel.dubsky@tul.cz>
2024-10-24 12:56:07 +0200

    #2 uprava

--- IS-Projekty/program002a-soucet-cifer/Program.cs
+++ IS-Projekty/program002a-soucet-cifer/Program.cs
@@ -4,7 +4,7 @@ while(again == "a") {
             Console.Clear();
             Console.WriteLine("*******************************");
             Console.WriteLine("**** Součet a součin cifer ****");
-            Console.WriteLine("******** Daniel Dubský*********");
+            Console.WriteLine("******** Daniel Dubský *********");
             Console.WriteLine("*******************************");
             Console.WriteLine();
             Console.WriteLine();
